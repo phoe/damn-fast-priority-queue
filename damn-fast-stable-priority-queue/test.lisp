eb7e1f9eb0dd03709c8c9f5b424652a892f430a0
Michał "phoe" Herda <phoe@disroot.org>
2021-01-15 19:14:34 +0100

    Add EXTEND-QUEUE-P

--- damn-fast-stable-priority-queue/test.lisp
+++ damn-fast-stable-priority-queue/test.lisp
@@ -2,6 +2,7 @@
 
 (defpackage #:damn-fast-stable-priority-queue/test
   (:use #:cl)
+  (:local-nicknames (#:q #:damn-fast-stable-priority-queue))
   (:export #:run))
 
 (in-package #:damn-fast-stable-priority-queue/test)
@@ -11,4 +12,19 @@
   (damn-fast-stable-priority-queue/test-same-priorities:run verbose)
   (when verbose (format t "~&;;; Testing with distinct priorities."))
   (damn-fast-stable-priority-queue/test-distinct-priorities:run verbose)
+  (when verbose (format t "~&;;; Testing an error scenario."))
+  (perform-error-test)
   (when verbose (format t "~&;;; Test complete.")))
+
+(defun perform-error-test ()
+  (let ((queue (q:make-queue 4 2 nil)))
+    (dotimes (i 4) (q:enqueue queue (princ-to-string i) i))
+    (flet ((perform ()
+             (multiple-value-bind (value error)
+                 (ignore-errors (q:enqueue queue "4" 4))
+               (assert (null value))
+               (assert (typep error 'q:queue-size-limit-reached))
+               (assert (eq queue (q:queue-size-limit-reached-queue error)))
+               (assert (string= "4"
+                                (q:queue-size-limit-reached-object error))))))
+      (dotimes (i 4) (perform)))))
